e07207e69d30fc228da325cfaa7144a940180bae
Roberto-R <robert.soor@gmail.com>
2021-09-28 16:47:08 +0200

    Added changes made during first real usage

--- init.m
+++ init.m
@@ -6,7 +6,18 @@ addpath('Functions/MuscleProperties');
 addpath('Functions/Reflexes');
 
 %% Load databus
-load('QualisysDataBus');
+QualisysDataBus_struct.M1x = double(0);
+QualisysDataBus_struct.M1y = double(0);
+QualisysDataBus_struct.M1z = double(0);
+QualisysDataBus_struct.M2x = double(0);
+QualisysDataBus_struct.M2y = double(0);
+QualisysDataBus_struct.M2z = double(0);
+QualisysDataBus_struct.M3x = double(0);
+QualisysDataBus_struct.M3y = double(0);
+QualisysDataBus_struct.M3z = double(0);
+
+QualisysDataBus = struct2bus(QualisysDataBus_struct, 'QualisysDataBus');
+clear QualisysDataBus_struct;
 
 %% Run WE2 init
 WE2_library_init;
